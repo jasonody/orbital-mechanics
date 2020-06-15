2fc7ee472d8e00c2b60957524f1e9b4c16fb30ac
Jason Ody <jasonody@users.noreply.github.com>
2020-06-14 18:15:14 -0700

    added Hubble space telescope

--- keplerian_classical_orbital_elements.py
+++ keplerian_classical_orbital_elements.py
@@ -20,13 +20,19 @@ if __name__ == '__main__':
   # random orbit
   c2 = [cb['radius'] + 3000.0, 0.3, 20.0, 0.0, 15.0, 40.0]
 
+  # Hubble space telescope
+  # https://www.heavens-above.com/orbit.aspx?satid=20580
+  c3 = [cb['radius'] + 538.0, 0.0002727, 28.4742, 0.0, 68.4651, 337.8887]
+
   # create orbit propagator
   op0 = OP(c0, tspan, dt, coes=True)
   op1 = OP(c1, tspan, dt, coes=True)
   op2 = OP(c2, tspan, dt, coes=True)
+  op3 = OP(c3, tspan, dt, coes=True)
 
   op0.propagate_orbit()
   op1.propagate_orbit()
   op2.propagate_orbit()
+  op3.propagate_orbit()
 
-  t.plot_n_orbits([op0.rs, op1.rs, op2.rs], labels=['ISS', 'GEO', 'Random'], show_plot=True)
+  t.plot_n_orbits([op0.rs, op1.rs, op2.rs, op3.rs], labels=['ISS', 'GEO', 'Random', 'Hubble'], show_plot=True)
